d4ca2d147069939516bb1a3b6a2bb178f204b82e
AgusWayer <aguswira246@gmail.com>
2024-01-23 06:52:43 +0800

    Add Category

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+
 	<link rel="stylesheet" type="text/css" href="style/index.css">
 	<title>DEEZ GALLERY</title>
 </head>
@@ -29,6 +32,7 @@
 			<i class="fa-solid fa-plus"></i>
 		</a>
 	</div>
+	<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 	<script src="script/masonry.pkgd.min.js">
 	</script>
 	<script type="text/javascript">
@@ -39,6 +43,10 @@
 		  columnWidth: 200,
 		  gutter: 10,
 		});
+		const swiper = new Swiper('.swiper',{
+			slidesPerView: 3,
+	      	spaceBetween: 10,
+			})
 	</script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	
--- view/index.php
+++ view/index.php
@@ -1,8 +1,55 @@
 <div class=" my-3">
-	<h3 class="text-center">Welcome, Agus Wira!</h3>
+	<h3 class="text-center"><?php 
+	if(isset($_SESSION['user'])){
+		echo "Welcome, ".$_SESSION['user']['namalengkap'];
+	}else{
+		echo "Hello";
+	}; ?></h3>
 	<p class="text-center">Let's Discover something new here!</p>
-	<div class="category">
-		<div></div>
+	<div class="categories mx-auto p-0 m-0 swiper">
+		<div class="swiper-wrapper">
+			<a href="" class="category d-flex align-items-center swiper-slide text-decoration-none">
+				<div class="img-container">
+					<img src="./assets/image1.jpg">
+					<img src="./assets/suguru.jpeg">
+					<img src="./assets/no-profile.webp">
+				</div>
+				<div class="">
+					<h4 class="text-white">Anime</h4>
+				</div>	
+			</a>
+			<a href="" class="category d-flex align-items-center swiper-slide text-decoration-none ">
+				<div class="img-container ">
+					<img src="./assets/image1.jpg">
+					<img src="./assets/suguru.jpeg">
+					<img src="./assets/no-profile.webp">
+				</div>
+				<div class="">
+					<h4 class="text-white">Anime</h4>
+				</div>
+			</a>
+			<a href="" class="category d-flex align-items-center swiper-slide text-decoration-none ">
+				<div class="img-container ">
+					<img src="./assets/image1.jpg">
+					<img src="./assets/suguru.jpeg">
+					<img src="./assets/no-profile.webp">
+				</div>
+				<div class="">
+					<h4 class="text-white">Anime</h4>
+				</div>
+			</a>
+			<a href="" class="category d-flex align-items-center swiper-slide text-decoration-none ">
+				<div class="img-container ">
+					<img src="./assets/image1.jpg">
+					<img src="./assets/suguru.jpeg">
+					<img src="./assets/no-profile.webp">
+				</div>
+				<div class="">
+					<h4 class="text-white">Anime</h4>
+				</div>
+			</a>
+		</div>
+		
 	</div>
 	<div class="grid mx-auto my-3">
 		<?php 
@@ -39,7 +86,7 @@
 						<div class="save p-2">
 							<a href="" class="btn btn-danger rounded-pill px-4 py-1">Save</a>
 						</div>	
-						<div class="  d-flex icons p-2">
+						<div class=" d-flex icons p-2">
 							<a href="./assets/suguru.jpeg" download="file.jpeg" class="icon-container text-white	"><i class="fa-solid fa-download fs-5 "></i></a>
 							<a href="./utiities/photo/php?action=delete&id=" class="icon-container text-white	"><i class="fa-solid fa-trash fs-5"></i></a>
 							
